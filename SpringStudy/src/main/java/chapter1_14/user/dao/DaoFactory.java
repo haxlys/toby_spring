34d059927263d84c429bc238c5107b6d0bd5e4cc
haxlys <haxlys@gmail.com>
2016-11-21 23:19:10 +0900

    자바 코드를 사용하여 DataSource를 적용

---
+++ SpringStudy/src/main/java/chapter1_14/user/dao/DaoFactory.java
@@ -0,0 +1,46 @@
+package chapter1_14.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import chapter1_14.user.dao.UserDao;
+
+import javax.sql.DataSource;
+
+public class DaoFactory {
+	
+	@Bean
+	public UserDao userDao(){
+		UserDao userDao = new UserDao();
+		userDao.setConnectionMaker(dataSource());
+		return userDao;
+	}
+	/*
+	 @Bean
+	 public UserDao userDao(){
+		UserDao userDao = new UserDao();
+		userDao.setConnectionMaker(connectionMaker());
+		return userDao;
+	}*/
+	
+	@Bean
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/test");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		
+		return dataSource;
+	}
+	/*
+	@Bean
+	public ConnectionMaker connectionMaker(){
+		return new DConnectionMaker();
+	}*/
+	
+	
+	
+	
+}
